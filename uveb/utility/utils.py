da4bad061d57ab6f2c0744590213e7db41d28ed3
Jaylan <natarichjay@gmail.com>
2019-08-24 10:09:27 +0800

    added utils

---
+++ uveb/utility/utils.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+
+def gen_key(l=24):
+    key = string.ascii_letters
+    res = ''.join(random.choice(key) for i in range(l)) + str(id)
+
+    # if VideoFetcher.fetch_by_track_id(res):
+    #     return gen_key(id)
+    return res
+
+
+def gen_code():
+    return random.randint(10000, 99999)
