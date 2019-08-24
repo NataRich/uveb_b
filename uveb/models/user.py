4e6ffb03576d2a586814c726efe7d306855e38f7
Jaylan <natarichjay@gmail.com>
2019-08-24 09:26:44 +0800

    added models

--- uveb/models/user.py
+++ uveb/models/user.py
@@ -1,1 +1,35 @@
 # User Model
+
+
+class UserModel:
+    """Represent a user"""
+
+    def __init__(self, username, email, identity, password_hash, date, id=None, num_videos=0,
+                 authenticated=0, thumb_image=None, medium_image=None):
+        self.username = username
+        self.email = email
+        self.identity = identity
+        self.password_hash = password_hash
+        self.date = date
+
+        self.id = id
+        self.num_videos = num_videos
+        self.authenticated = authenticated
+        self.thumb_image = thumb_image
+        self.medium_image = medium_image
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'identity': self.identity,
+            'password_hash': self.password_hash,
+            'date': self.date,
+            'numVideos': self.num_videos,
+            'authenticated': self.authenticated,
+            'thumbImage': self.thumb_image,
+            'mediumImage': self.medium_image
+        }
+
+
